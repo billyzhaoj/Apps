18ff81af9539a55a0b8bdac51e9bfb4e69bee6dd
Billy Zhao <william_zhao@berkeley.edu>
2019-10-25 01:36:56 -0700

    openthread readd

--- heatmap/main.c
+++ heatmap/main.c
@@ -15,15 +15,16 @@
 
 #include <stdio.h>
 #include <rtt_stdio.h>
+#include "ot.h"
+#include <openthread/udp.h>
+#include <openthread/cli.h>
+#include <openthread/openthread.h>
 #include "main.h"
 //#include "periph/dmac.h"
 #include "sched.h"
-#include "xtimer.h"
 #include "periph/adc.h"
 #include "periph/i2c.h"
 #include "periph/spi.h"
-#include "at86rf2xx.h"
-#include "at86rf2xx_params.h"
 
 #ifndef SAMPLE_INTERVAL
 #define SAMPLE_INTERVAL (20000000UL)
@@ -37,53 +38,6 @@
 #define CHANNEL_OFFSET 11
 #define CHANNEL_MOD    16
 #include "debug.h"
-
-static at86rf2xx_t at86rf2xx_dev;
-static void _event_cb(netdev_t *dev, netdev_event_t event) {
-    switch (event) {
-        case NETDEV_EVENT_ISR:
-            {
-                /* radio_rx_msg.type = OPENTHREAD_NETDEV_MSG_TYPE_EVENT; */
-                /* radio_rx_msg.content.ptr = dev; */
-                /* radio_rx_msg.content.value = 1; */
-/* #ifdef MODULE_OPENTHREAD_FTD */
-                /* unsigned irq_state = irq_disable(); */
-                /* ((at86rf2xx_t *)dev)->pending_irq++; */
-                /* irq_restore(irq_state); */
-/* #endif */
-                /* if (msg_send(&radio_rx_msg, openthread_get_main_pid()) <= 0) { */
-                /*     printf("ot_main: possibly lost radio interrupt.\n"); */
-/* #ifdef MODULE_OPENTHREAD_FTD */
-                /*     unsigned irq_state = irq_disable(); */
-                /*     ((at86rf2xx_t *)dev)->pending_irq--; */
-                /*     irq_restore(irq_state); */
-/* #endif */
-                /* } */
-                break;
-            }
-        case NETDEV_EVENT_ISR2:
-            {
-                /* radio_tx_msg.type = OPENTHREAD_NETDEV_MSG_TYPE_EVENT; */
-                /* radio_tx_msg.content.ptr = dev; */
-                /* radio_tx_msg.content.value = 0; */
-                /* if (msg_send(&radio_tx_msg, openthread_get_task_pid()) <= 0) { */
-                /*     printf("ot_task: possibly lost radio interrupt.\n"); */
-                /* } */
-                break;
-            }
-        case NETDEV_EVENT_RX_COMPLETE:
-            //recv_pkt(openthread_get_instance(), dev);
-            break;
-        case NETDEV_EVENT_TX_COMPLETE:
-        case NETDEV_EVENT_TX_COMPLETE_DATA_PENDING:
-        case NETDEV_EVENT_TX_NOACK:
-        case NETDEV_EVENT_TX_MEDIUM_BUSY:
-            //sent_pkt(openthread_get_instance(), event);
-            break;
-        default:
-            break;
-    }
-}
 int main(void)
 {
 #if DMAC_ENABLE
@@ -92,34 +46,23 @@ int main(void)
     i2c_set_dma_channel(I2C_0,DMAC_CHANNEL_I2C);
     spi_set_dma_channel(0,DMAC_CHANNEL_SPI_TX,DMAC_CHANNEL_SPI_RX);
 #endif 
+    DEBUG("This a test for OpenThread\n");
     xtimer_usleep(300000ul);
     xtimer_ticks32_t last_wakeup = xtimer_now(); 
-
-
-    at86rf2xx_setup(&at86rf2xx_dev, &at86rf2xx_params[0]);
-    netdev_t *netdev = (netdev_t *) &at86rf2xx_dev; 
-    netdev->driver->init(netdev);
-    netdev->event_callback = _event_cb;
-    netopt_enable_t enable = NETOPT_ENABLE;
-    netdev->driver->set(netdev, NETOPT_RX_END_IRQ, &enable, sizeof(enable));
-    netdev->driver->set(netdev, NETOPT_TX_END_IRQ, &enable, sizeof(enable));
+    netdev_t* netdev =  openthread_get_netdev();
     uint8_t chan = CHANNEL_OFFSET; 
-    uint16_t panid = 0xbeef;   
     netopt_t state;
-    netdev->driver->set(netdev,NETOPT_NID, &panid,sizeof(panid));
     while (1) {
         xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
-        //int res = netdev->driver->set(netdev,NETOPT_CHANNEL, &chan,sizeof(chan));
-        
-        //DEBUG("channel set ret code: %d \n", res);
-        state = NETOPT_STATE_IDLE;
+        otError err = otLinkSetChannel(openthread_get_instance(), chan);
+        DEBUG("otError: %d \n", err);
+        state = AT86RF2XX_STATE_RX_ON;
         netdev->driver->set(netdev, NETOPT_STATE, &state,sizeof(state));
         netdev->driver->get(netdev, NETOPT_STATE, &state,sizeof(state));
         DEBUG("NETOPT STATE: %d\n", state);
-        
+
         int res = netdev->driver->get(netdev,NETOPT_ED,NULL,0);
-        
-        DEBUG("CHANNEL: %d ED READING: %d\n", chan, res);
+        DEBUG("CHANNEL: %d ED READING: %d\n", otLinkGetChannel(openthread_get_instance()), res);
         chan = (chan - 10) % CHANNEL_MOD + CHANNEL_OFFSET;
     }
     return 0;
